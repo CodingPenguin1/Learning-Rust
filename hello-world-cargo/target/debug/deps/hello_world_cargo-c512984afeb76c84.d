86168494e998736a6b9211286b968518acf47be0
Ryan Slater <ryan.j.slater.2@gmail.com>
2021-07-26 15:20:49 -0400

    tried to remove target/

--- hello-world-cargo/target/debug/deps/hello_world_cargo-c512984afeb76c84.d
+++
@@ -1,5 +0,0 @@
-/home/stark/Documents/Learning-Rust/hello-world-cargo/target/debug/deps/hello_world_cargo-c512984afeb76c84: src/main.rs
-
-/home/stark/Documents/Learning-Rust/hello-world-cargo/target/debug/deps/hello_world_cargo-c512984afeb76c84.d: src/main.rs
-
-src/main.rs:
--- hello-world-cargo/target/debug/hello-world-cargo.d
+++
@@ -1,1 +0,0 @@
-/home/stark/Documents/Learning-Rust/hello-world-cargo/target/debug/hello-world-cargo: /home/stark/Documents/Learning-Rust/hello-world-cargo/src/main.rs
