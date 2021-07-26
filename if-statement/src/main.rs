fc273ce1b09a6912e736a88d4acecd8259ef667e
Ryan Slater <ryan.j.slater.2@gmail.com>
2021-07-26 15:47:41 -0400

    If statements

---
+++ if-statement/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let n = 20;
+
+    if n == 30 {
+        println!("n == 30");
+    } else if n > 30 {
+        println!("n > 30");
+    } else {
+        println!("n < 30");
+    }
+}
